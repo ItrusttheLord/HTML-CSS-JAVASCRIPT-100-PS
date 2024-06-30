8b5db73176be940bb23b9a0bf9e7927c327facf3
Yender Reynoso <kokihidalgo18@gmail.com>
2024-06-30 16:31:55 -0400

    Random Emojies Project 21

---
+++ 21.Random Emojies/script.js
@@ -0,0 +1,35 @@
+const btn = document.querySelector("#emoji");
+
+const emojis = [
+  "🐵",
+  "🐒",
+  "🦍",
+  "🦧",
+  "🐶",
+  "🐕",
+  "🦮",
+  "🐕‍🦺",
+  "🐩",
+  "🐺",
+  "🦊",
+  "🦝",
+  "🐱",
+  "🐈",
+  "🐈‍⬛",
+  "🦁",
+  "🐯",
+  "🐅",
+  "🐆",
+  "🐴",
+  "🐎",
+  "🦄",
+  "🦓",
+  "🦌",
+  "🦬",
+  "🐮",
+  "🐂",
+];
+
+btn.addEventListener("mouseover", () => {
+  btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
+});
