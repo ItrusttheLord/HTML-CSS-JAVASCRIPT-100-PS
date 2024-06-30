40fdc7d077825c22776b9d890690f56addb9d621
Yender Reynoso <kokihidalgo18@gmail.com>
2024-06-30 16:01:43 -0400

    Animated Nav Project 20

---
+++ 20.Animated Navigator/script.js
@@ -0,0 +1,6 @@
+// toggle
+const toggle = document.getElementById("toggle");
+// nav
+const nav = document.getElementById("nav");
+
+toggle.addEventListener("click", () => nav.classList.toggle("active"));
